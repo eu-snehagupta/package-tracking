e1805acd6fbf845aea283c16dc7f9eba874e8d96
Sneha <sneha.gupta@gmail.com>
2020-11-07 21:50:06 +0100

    Add track-form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 //components
 import Header from "./components/organisms/Header";
+import TrackForm from "./components/molecules/TrackForm";
 
 //style
 import "./css/style.css"
-function App() {
+
+export default function App() {
   return (
     <div className="App">
       <Header />
+      <TrackForm />
     </div>
   );
 }
-
-export default App;
